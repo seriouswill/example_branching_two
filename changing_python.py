7d6830d4b993ea1ed2f194c3996e7077034ae10f
Will O'Connell <william.oconnell@makers.tech>
2024-01-29 15:35:52 +0000

    added car class

--- changing_python.py
+++ changing_python.py
@@ -1,4 +1,12 @@
 
+class Car():
+    def __init__(self) -> None:
+        self.wheels = 7
+        self.speed = 0
+
+    def accelarate(self, increase):
+        self.speed += increase
+        print(f"You have increased thew speed to {self.speed}")
 
 
 print("Hello")
