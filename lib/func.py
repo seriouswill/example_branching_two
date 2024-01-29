ae812d1fc01ee82ad333afce56e2a3fe13ea19cc
Will O'Connell <william.oconnell@makers.tech>
2024-01-29 15:07:42 +0000

    first comit

---
+++ lib/func.py
@@ -0,0 +1,4 @@
+
+def function_one(num):
+    num = num + 2
+    return num
---
+++ tests/test_func.py
@@ -0,0 +1,6 @@
+from func import *
+
+def test_func_works():
+    assert function_one(2) == 4
+    assert function_one(4) == 6
+    assert function_one(7) == 9
